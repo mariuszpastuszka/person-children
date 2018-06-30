76fd4c20b9f0d515bad3c3124c20e1e806769557
mariuszpastuszka <mpastuszka@gmail.com>
2018-06-30 14:21:44 +0200

    [add] - adding first dao method

--- src/main/java/pl/mpas/parent_children/dao/PersonDaoImpl.java
+++ src/main/java/pl/mpas/parent_children/dao/PersonDaoImpl.java
@@ -3,6 +3,9 @@ package pl.mpas.parent_children.dao;
 import pl.mpas.parent_children.model.Person;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class PersonDaoImpl implements PersonDao {
@@ -14,7 +17,27 @@ public class PersonDaoImpl implements PersonDao {
 
     @Override
     public boolean savePerson(Person person) {
-        return false;
+        String insertQuery = "INSERT INTO PERSON (NAME, SURNAME, AGE, ADULT_OR_CHILD, SEX) " +
+                " VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = dbConnection.prepareStatement(insertQuery);
+
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setString(2, person.getSurname());
+            preparedStatement.setInt(3, person.getAge());
+            preparedStatement.setString(4, "" + person.getAdultOrChild().getMarker());
+            preparedStatement.setString(5, "" + person.getSex().getSex());
+            int rowAffected = preparedStatement.executeUpdate();
+            System.out.println("Number of added records: " + rowAffected);
+            ResultSet result = preparedStatement.getGeneratedKeys();
+            if (result.next()) {
+                result.getInt(1);
+            };
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
     @Override
